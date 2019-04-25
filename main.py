a838f7daf4cd85c39d63fc9f40c6bf820d368adc
zengyuhuei <zengyuhuei@gmail.com>
2019-04-25 17:39:55 +0800

    debug the route of html

--- main.py
+++ main.py
@@ -62,18 +62,6 @@ def index():
     return render_template('index.html')
 
 
-
-
-    
-@app.route('/testPost', methods=['POST'])
-def hahaha():
-    data = request.get_json()
-    print("name = " + data["name"])
-    print("age = " + str(data["age"]))
-    return 'Hello World!: ' 
-
-
-
 @app.route('/add_info_to_db', methods=['POST'])
 def add_info_to_db():
     """
@@ -132,5 +120,64 @@ def get_info():
         response["status"] = "error"
         print(str(e))
     return str(response)
+
+@app.route('/bus_driver_change_password', methods=['GET'])
+def bus_driver_change_password():
+    return render_template('bus_driver_change_password.html')
+
+@app.route('/bus_driver_emergency_return', methods=['GET'])
+def bus_driver_emergency_return():
+    return render_template('bus_driver_emergency_return.html')
+
+@app.route('/bus_driver_people_number_return', methods=['GET'])
+def bus_driver_people_number_return():
+    return render_template('bus_driver_people_number_return.html')
+
+@app.route('/bus_driver_personal_basic_information', methods=['GET'])
+def bus_driver_personal_basic_information():
+    return render_template('bus_driver_personal_basic_information.html')
+
+@app.route('/add_busdriver', methods=['GET'])
+def add_busdriver():
+    return render_template('add_busdriver.html', methods=['GET'])
+
+@app.route('/add_or_revise_shift', methods=['GET'])
+def add_or_revise_shift():
+    return render_template('add_or_revise_shift.html', methods=['GET'])
+
+@app.route('/bus_information', methods=['GET'])
+def bus_information():
+    return render_template('bus_information.html')
+
+@app.route('/change_password', methods=['GET'])
+def change_password():
+    return render_template('change_password.html', methods=['GET'])
+
+@app.route('/Emergency_reception', methods=['GET'])
+def Emergency_reception():
+    return render_template('Emergency_reception.html')
+
+@app.route('/historical_record', methods=['GET'])
+def historical_record():
+    return render_template('historical_record.html')
+
+@app.route('/Human_dispatch', methods=['GET'])
+def Human_dispatch():
+    return render_template('Human_dispatch.html')
+
+@app.route('/Personal_basic_information', methods=['GET'])
+def Personal_basic_information():
+    return render_template('Personal_basic_information.html')
+
+@app.route('/revise_path', methods=['GET'])
+def revise_path():
+    return render_template('revise_path.html', methods=['GET'])
+
+@app.route('/timely_bus_information', methods=['GET'])
+def timely_bus_information():
+    return render_template('timely_bus_information.html')
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
