62b1171aed456beacc1ba6123193264c307fefee
zengyuhuei <zengyuhuei@gmail.com>
2019-04-29 01:26:45 +0800

    remove pycache

--- model.py
+++ model.py
@@ -20,15 +20,16 @@ class Model:
      # modify info
     def modify_info_to_db(self, data):
         email = dict()
-        
         client = pymongo.MongoClient('mongodb://user:870215@140.121.198.84:27017/')
         db = client["KeelungBusSystem"]
         email['email'] = data['email']
+        account = data['email']
         print(email)
         del data['email']
         print(data)
         result = db['info'].update_one(email , { "$set": data })
-        #result['identify'] = 
+        result['identify'] =  db["auth"].find_one({"account" : account})
+        print(result)
         return result
     
     #get info from db    
