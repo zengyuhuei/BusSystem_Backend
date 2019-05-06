397a85e6861e0404c83bb71b9abe00ecaa1b84da
mf99250 <mf99250@gmail.com>
2019-05-06 19:57:41 +0800

    GPS

--- static/js/bus_driver_people_number_return.js
+++ static/js/bus_driver_people_number_return.js
@@ -36,48 +36,6 @@ $(document).ready(function(){
 	});
 });
 
-$(document).on('click','#start',function(){
-	console.log("oqo");
-	start();
-});
-
-function start()
-{
-	console.log("ouo");
-	/*var date = new Date(); //日期对象 
-	var now = ""; 
-	now = date.getFullYear()+"-"; //读英文就行了 
-	now = now + (date.getMonth()+1)+"-"; //取月的时候取的是当前月-1如果想取当前月+1就可以了 
-	now = now + date.getDate()+"T"; 
-	now = now + date.getHours()+":"; 
-	now = now + date.getMinutes()+":00.000Z";
-
-	console.log(now);
-	//setInterval("document.getElementById('datetime').innerHTML=new Date().toLocaleString();", 1000);*/
-
-
-	$.ajax({
-		type: "POST",
-		data: "json",
-		dataType: "json",
-		contentType : 'application/json',
-		url: "http://140.121.198.84:3000/setbusGPS",
-		data:JSON.stringify({
-			"email": localStorage.account,
-			"start_time": "datetime.datetime(1900, 1, 1, 7, 0)",
-			"lat": lat,
-			"lng": lng
-		}),
-		success: function(response) {
-			console.log(response);
-		},
-		error: function(xhr, type) {
-			console.log("hehehe");
-		}
-	});
-	setTimeout("start()",3000); 
-}
-
 $(document).on('click','#end',function(){
 	console.log("ouo");
 
