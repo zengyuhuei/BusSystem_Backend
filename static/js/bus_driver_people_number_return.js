8aa900085ee64b79c771e3fbbffa80c93070bd03
zengyuhuei <zengyuhuei@gmail.com>
2019-05-17 04:39:54 +0800

    a

--- static/js/bus_driver_people_number_return.js
+++ static/js/bus_driver_people_number_return.js
@@ -36,12 +36,13 @@ $(document).ready(function(){
 	$(document).on('click','.save',function(){
 		var people_number1 = 0;
 		console.log(people_number1);
-		people_number1 += parseInt($('.count').val()) - parseInt($('.count1').val());
+		people_number1 = parseInt(localStorage.getItem('peoplenum')) + parseInt($('.count').val()) - parseInt($('.count1').val());
 		console.log(localStorage.getItem('peoplenum'));
 		console.log(people_number1);
 		console.log($('.count').val());
 		console.log($('.count1').val());
 		//$('#people_number').val(parseInt(localStorage.getItem('peoplenum') + $('.count').val()) - parseInt($('.count1').val()) )
+		localStorage.setItem('peoplenum',people_number1);
 		if(localStorage.getItem('peoplenum')>0)
 			document.getElementById("people_number").innerHTML = localStorage.getItem('peoplenum');
 		else
@@ -50,7 +51,7 @@ $(document).ready(function(){
 			localStorage.setItem('peoplenum',0);
 			people_number1 = 0;
 		}
-		localStorage.setItem('peoplenum',people_number1);
+		
 		$('.count1').val(0);
 		$('.count').val(0);
 	});
