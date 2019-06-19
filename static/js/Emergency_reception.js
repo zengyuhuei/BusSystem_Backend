ba5bf65a2266198b16f99af5bb9331aa326749c2
YuHeng <k28998989@gmail.com>
2019-06-20 01:44:42 +0800

    c

--- static/js/Emergency_reception.js
+++ static/js/Emergency_reception.js
@@ -76,7 +76,7 @@ function setDriver()
             for(var j = 0; j < marker1.length ; j++){
               console.log(marker1[j]['driver'])
               var now = localStorage.getItem(marker1[j]['driver'])
-              var message = "發生事故<br>司機:"+marker1[j]['driver']+"<br>時間:"+marker1[j]["arrive_time"]+"<br>狀況:"+now);
+              var message = "發生事故<br>司機:"+marker1[j]['driver']+"<br>時間:"+marker1[j]["arrive_time"]+"<br>狀況:"+now;
               console.log(marker1[j])
               console.log(message)
               var myLatLng = {lat:parseFloat(marker1[j]['lat']),lng:parseFloat(marker1[j]['lng'])};
